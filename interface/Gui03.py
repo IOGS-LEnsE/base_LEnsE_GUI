1d87479d3466c683417821e811ae502c9f2243a0
Julien Villemejane <julien.villemejane@institutoptique.fr>
2023-12-19 20:46:30 +0100

    Update Gui1 and Gui3

--- interface/Gui03.py
+++ interface/Gui03.py
@@ -3,9 +3,8 @@
 
 This GUI is developped in Python 3 and is based on 
 PyQt6 for graphical objects.
-It divided in :
-- 1 main area (QWidget)
-- 1 left menu (QWidget)
+
+This example shows how to use events on buttons to update labels.
 
 ---------------------------------------
 (c) 2023 - LEnsE - Institut d'Optique
@@ -24,7 +23,7 @@ Authors
 # Libraries to import
 import sys
 
-from PyQt6.QtWidgets import QApplication, QMainWindow, QGridLayout, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLabel, QPushButton
 from PyQt6.QtGui import QIcon
 
 from elements.SimpleWidget import SimpleWidget
@@ -54,25 +53,42 @@ class MainWindow(QMainWindow):
         self.main_widget = QWidget()
 
         # Main Layout
-        self.main_layout = QGridLayout()
-        # Left area of size 1 / 5 of the width
-        self.main_layout.setColumnStretch(0, 1)
-        # Left area of size 4 / 5 of the width  
-        self.main_layout.setColumnStretch(1, 4)
+        self.main_layout = QVBoxLayout()
         self.main_widget.setLayout(self.main_layout)
         self.setCentralWidget(self.main_widget)
         
-        # Main Area
-        self.main_area = SimpleWidget(title='Main Area', 
-                    background_color='white',
-                    text_color='red')
+        # Buttons
+        self.button1 = QPushButton('Click 1')
+        self.button1.clicked.connect(self.button1_action)
+        self.button2 = QPushButton('Click 2')
+        self.button2.clicked.connect(self.button2_action)
+        self.button2.setEnabled(False)
+        
+        # Labels
+        self.label1 = QLabel('Nothing 1')
+        self.label2 = QLabel('Nothing 2')
+        
+        # Integration in the main area
+        self.main_layout.addWidget(self.button1)
+        self.main_layout.addWidget(self.label1)
+        self.main_layout.addWidget(self.button2)
+        self.main_layout.addWidget(self.label2)
+        
 
-        # Left Main Menu
-        self.main_menu = SimpleWidget(title='Left Menu')
+    def button1_action(self):
+        """
+        Action to process when button1 is clicked.
+        """
+        self.button2.setEnabled(True)  # Button2 is now enabled
+        self.label1.setText('Click on the 2nd button')
 
-        # Include graphical elements in the window application
-        self.main_layout.addWidget(self.main_menu, 0, 0)
-        self.main_layout.addWidget(self.main_area, 0, 1)
+
+    def button2_action(self):
+        """
+        Action to process when button2 is clicked.
+        """
+        self.button1.setEnabled(False)  # Button1 is now disabled
+        self.label2.setText('Button 2 was clicked')
 
 
 # -------------------------------
